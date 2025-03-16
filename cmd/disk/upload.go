78032e65fdbce02bac71f006c32427b02444e7b7
zzzhr <zzzhr@hotmail.com>
2025-03-16 23:41:19 +0800

    add new pubUserFile upload function

--- cmd/disk/upload.go
+++ cmd/disk/upload.go
@@ -4,40 +4,28 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package disk
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
 
-	//"time"
 	pubUserFile "github.com/city404/v6-public-rpc-proto/go/v6/userfile"
-	"github.com/ipfs/boxo/blockservice"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-
-	"github.com/ipfs/boxo/ipld/unixfs/importer/balanced"
-
-	//pubUserFile "github.com/city404/v6-public-rpc-proto/go/v6/userfile"
 	"github.com/halalcloud/golang-sdk/auth"
 	"github.com/halalcloud/golang-sdk/constants"
-	"github.com/halalcloud/golang-sdk/ipfs"
-	"github.com/halalcloud/golang-sdk/pkg/print"
 	"github.com/halalcloud/golang-sdk/utils"
-	"github.com/ipfs/boxo/ipld/merkledag"
-
-	// ipld "github.com/ipfs/go-ipld-format"
+	"github.com/ipfs/go-cid"
 	"github.com/spf13/cobra"
 	"github.com/zzzhr1990/go-common-entity/userfile"
-
-	//"google.golang.org/grpc/codes"
-	//"google.golang.org/grpc/status"
-	chunker "github.com/ipfs/boxo/chunker"
-	"github.com/ipfs/boxo/ipld/unixfs/importer/helpers"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // UploadCmd represents the mkdir command
@@ -52,7 +40,6 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		currentDir := utils.GetCurrentDir()
 		serv, err := auth.NewAuthService(constants.AppID, constants.AppVersion, constants.AppSecret, "")
 		if err != nil {
 			fmt.Println(err)
@@ -81,8 +68,78 @@ to quickly create a Cobra application.`,
 
 		newDir := userfile.NewFormattedPath(utils.GetCurrentDir()).GetPath()
 		newDir = strings.TrimSuffix(newDir, "/") + "/" + fileInfo.Name()
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+		defer cancel()
+		result, err := pubUserFile.NewPubUserFileClient(serv.GetGrpcConnection()).CreateUploadTask(ctx, &pubUserFile.File{
+			// Parent: &pubUserFile.File{Path: currentDir},
+			Path: newDir,
+			//ContentIdentity: args[1],
+			Size: fileInfo.Size(),
+		})
+		if err != nil {
+			status, ok := status.FromError(err)
+			if ok {
+				if status.Code() == codes.NotFound {
+					fmt.Printf("Directory [%s] not found, back to root.\n", utils.GetCurrentDir())
+					utils.SetCurrentDir("/")
+					return
+				}
+			}
+			fmt.Println(err)
+			return
+		}
+		if result.Created {
+			fmt.Printf("Upload file created\n")
+			return
+		}
+		fmt.Printf("Upload task started, block size: %d -> %s\n", result.BlockSize, result.Task)
+		bufferSize := int(result.BlockSize)
+		buffer := make([]byte, bufferSize)
+		slicesList := make([]string, 0)
+		codec := uint64(cid.Raw)
+		if result.BlockCodec > 0 {
+			codec = uint64(result.BlockCodec)
+		}
+		mhType := uint64(0x12)
+		if result.BlockHashType > 0 {
+			mhType = uint64(result.BlockHashType)
+		}
+		prefix := cid.Prefix{
+			Codec:    codec,
+			MhLength: -1,
+			MhType:   mhType,
+			Version:  1,
+		}
+		// read file
+		fi, err := os.Open(path)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer fi.Close()
+		for {
+			n, err := fi.Read(buffer)
+			if n > 0 {
+				data := buffer[:n]
+				uploadCid, err := postFileSlice(data, result.Task, result.UploadAddress, prefix)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				slicesList = append(slicesList, uploadCid.String())
+			}
+			if err == io.EOF || n == 0 {
+				break
+			}
+		}
+		newFile, err := makeFile(slicesList, result.Task, result.UploadAddress)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("File uploaded, cid: %s\n", newFile.ContentIdentity)
 
-		helpers.BlockSizeLimit = constants.IpfsDefaultBlockSize * 2
+		// helpers.BlockSizeLimit = constants.IpfsDefaultBlockSize * 2
 		/*
 			sp := print.Spinner(os.Stdout, "Upload File [%s] ...", newDir)
 			ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
@@ -107,106 +164,7 @@ to quickly create a Cobra application.`,
 			}
 			sp(true)
 		*/
-		fi, err := os.Open(path)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer fi.Close()
-		reader := fi
 
-		print.InfoStatusEvent(os.Stdout, "Starting upload [%s].", path)
-		bserv := blockservice.New(&ipfs.NullBlockstore{}, nil)
-		dserv := merkledag.NewDAGService(bserv)
-		// -^-^-
-		chnk := chunker.NewSizeSplitter(reader, constants.IpfsDefaultBlockSize)
-		params := helpers.DagBuilderParams{
-			Dagserv:    dserv,
-			RawLeaves:  true,
-			Maxlinks:   constants.IpfsDefaultBlockSize,
-			NoCopy:     false,
-			CidBuilder: merkledag.V1CidPrefix(),
-			//FileMode:    adder.FileMode,
-			//FileModTime: adder.FileMtime,
-		}
-
-		db, err := params.New(chnk)
-		if err != nil {
-			print.FailureStatusEvent(os.Stdout, "New DagBuilder error: %s", err)
-			return
-		}
-		//if adder.Trickle {
-		//	nd, err = trickle.Layout(db)
-		//} else {
-		nd, err := balanced.Layout(db)
-		//}
-		if err != nil {
-			print.FailureStatusEvent(os.Stdout, "Layout error: %s", err)
-			return
-		}
-
-		cid := nd.Cid()
-		print.SuccessStatusEvent(os.Stdout, "CID: %s", cid.String())
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
-		defer cancel()
-		result, err := pubUserFile.NewPubUserFileClient(serv.GetGrpcConnection()).CreateUploadTask(ctx, &pubUserFile.File{
-			// Parent: &pubUserFile.File{Path: currentDir},
-			Path:            newDir,
-			ContentIdentity: cid.String(),
-			Size:            fileInfo.Size(),
-			//ContentIdentity: args[1],
-		})
-		if err != nil {
-			status, ok := status.FromError(err)
-			if ok {
-				if status.Code() == codes.NotFound {
-					fmt.Printf("Directory [%s] not found, back to root.\n", currentDir)
-					utils.SetCurrentDir("/")
-					return
-				}
-			}
-			fmt.Println(err)
-			return
-		}
-		if result.Created {
-			print.SuccessStatusEvent(os.Stdout, "Upload Success. (Fast Upload)")
-			return
-		}
-		print.InfoStatusEvent(os.Stdout, "Starting upload [%s => %s].", result.UploadAddress, result.Task)
-		httpClient := &http.Client{
-			Timeout: time.Second * 90,
-		}
-		reader.Seek(0, io.SeekStart)
-		uploadDagService := merkledag.NewDAGService(blockservice.New(ipfs.NewUploadBlockstore(httpClient, result.UploadAddress, result.Task, cid.String()), nil))
-		params = helpers.DagBuilderParams{
-			Dagserv:    uploadDagService,
-			RawLeaves:  true,
-			Maxlinks:   constants.IpfsDefaultBlockSize,
-			NoCopy:     false,
-			CidBuilder: merkledag.V1CidPrefix(),
-			//FileMode:    adder.FileMode,
-			//FileModTime: adder.FileMtime,
-		}
-		chnk = chunker.NewSizeSplitter(reader, constants.IpfsDefaultBlockSize)
-
-		db, err = params.New(chnk)
-		if err != nil {
-			print.FailureStatusEvent(os.Stdout, "New UploadDagBuilder error: %s", err)
-			return
-		}
-		//if adder.Trickle {
-		//	nd, err = trickle.Layout(db)
-		//} else {
-		nd, err = balanced.Layout(db)
-		//}
-		if err != nil {
-			print.FailureStatusEvent(os.Stdout, "Layout error: %s", err)
-			return
-		}
-
-		newCid := nd.Cid()
-		print.SuccessStatusEvent(os.Stdout, "Upload CID: %s", newCid.String())
-		print.SuccessStatusEvent(os.Stdout, "Upload Success.")
 	},
 }
 
@@ -222,4 +180,118 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// mkdirCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+func makeFile(fileSlice []string, taskID string, uploadAddress string) (*pubUserFile.File, error) {
+	accessUrl := uploadAddress + "/" + taskID
+	getTimeOut := time.Minute * 2
+	u, err := url.Parse(accessUrl)
+	if err != nil {
+		return nil, err
+	}
+	n, _ := json.Marshal(fileSlice)
+	httpRequest := http.Request{
+		Method: http.MethodPost,
+		URL:    u,
+		Header: map[string][]string{
+			"Accept":         {"application/json"},
+			"Content-Type":   {"application/octet-stream"},
+			"Content-Length": {fmt.Sprintf("%d", len(fileSlice))},
+		},
+		Body: io.NopCloser(bytes.NewReader(n)),
+	}
+	httpClient := http.Client{
+		Timeout: getTimeOut,
+	}
+	httpResponse, err := httpClient.Do(&httpRequest)
+	if err != nil {
+		return nil, err
+	}
+	defer httpResponse.Body.Close()
+	if httpResponse.StatusCode != http.StatusOK && httpResponse.StatusCode != http.StatusCreated {
+		b, _ := io.ReadAll(httpResponse.Body)
+		fmt.Println(string(b))
+		return nil, fmt.Errorf("mk file slice failed, status code: %d", httpResponse.StatusCode)
+	}
+	b, _ := io.ReadAll(httpResponse.Body)
+	var result *pubUserFile.File
+	err = json.Unmarshal(b, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+func postFileSlice(fileSlice []byte, taskID string, uploadAddress string, preix cid.Prefix) (cid.Cid, error) {
+	// 1. sum file slice
+	newCid, err := preix.Sum(fileSlice)
+	if err != nil {
+		return cid.Undef, err
+	}
+	// 2. post file slice
+	sliceCidString := newCid.String()
+	// /{taskID}/{sliceID}
+	accessUrl := uploadAddress + "/" + taskID + "/" + sliceCidString
+	getTimeOut := time.Second * 30
+	// get {accessUrl} in {getTimeOut}
+	u, err := url.Parse(accessUrl)
+	if err != nil {
+		return cid.Undef, err
+	}
+	// header: accept: application/json
+	// header: content-type: application/octet-stream
+	// header: content-length: {fileSlice.length}
+	// header: x-content-cid: {sliceCidString}
+	// header: x-task-id: {taskID}
+	httpRequest := http.Request{
+		Method: http.MethodGet,
+		URL:    u,
+		Header: map[string][]string{
+			"Accept": {"application/json"},
+		},
+	}
+	httpClient := http.Client{
+		Timeout: getTimeOut,
+	}
+	httpResponse, err := httpClient.Do(&httpRequest)
+	if err != nil {
+		return cid.Undef, err
+	}
+	if httpResponse.StatusCode != http.StatusOK {
+		return cid.Undef, fmt.Errorf("upload file slice failed, status code: %d", httpResponse.StatusCode)
+	}
+	var result bool
+	b, err := io.ReadAll(httpResponse.Body)
+	if err != nil {
+		return cid.Undef, err
+	}
+	err = json.Unmarshal(b, &result)
+	if err != nil {
+		return cid.Undef, err
+	}
+	if result {
+		return newCid, nil
+	}
+
+	httpRequest = http.Request{
+		Method: http.MethodPost,
+		URL:    u,
+		Header: map[string][]string{
+			"Accept":         {"application/json"},
+			"Content-Type":   {"application/octet-stream"},
+			"Content-Length": {fmt.Sprintf("%d", len(fileSlice))},
+		},
+		Body: io.NopCloser(bytes.NewReader(fileSlice)),
+	}
+	httpResponse, err = httpClient.Do(&httpRequest)
+	if err != nil {
+		return cid.Undef, err
+	}
+	defer httpResponse.Body.Close()
+	if httpResponse.StatusCode != http.StatusOK && httpResponse.StatusCode != http.StatusCreated {
+		b, _ := io.ReadAll(httpResponse.Body)
+		fmt.Println(string(b))
+		return cid.Undef, fmt.Errorf("upload file slice failed, status code: %d", httpResponse.StatusCode)
+	}
+	//
+
+	return newCid, nil
 }
--- go.mod
+++ go.mod
@@ -1,59 +1,40 @@
 module github.com/halalcloud/golang-sdk
 
-go 1.24.0
+go 1.24.1
 
 require (
-	github.com/briandowns/spinner v1.23.0
-	github.com/city404/v6-public-rpc-proto/go v0.0.0-20250224145256-945e71067cf6
-	github.com/dgraph-io/badger/v4 v4.3.1
+	github.com/briandowns/spinner v1.23.2
+	github.com/city404/v6-public-rpc-proto/go v0.0.0-20250315161120-1f7b705db53d
+	github.com/dgraph-io/badger/v4 v4.6.0
 	github.com/dustin/go-humanize v1.0.1
 	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
-	github.com/fatih/color v1.14.1
+	github.com/fatih/color v1.18.0
 	github.com/google/uuid v1.6.0
-	github.com/ipfs/boxo v0.27.4
-	github.com/ipfs/go-block-format v0.2.0
-	github.com/ipfs/go-cid v0.4.1
+	github.com/ipfs/go-cid v0.5.0
 	github.com/mdp/qrterminal/v3 v3.2.0
 	github.com/olekukonko/tablewriter v0.0.5
-	github.com/pion/webrtc/v4 v4.0.1
-	github.com/spf13/cobra v1.8.1
-	github.com/spf13/viper v1.17.0
-	github.com/zzzhr1990/go-common-entity v0.0.0-20221216044934-fd1c571e3a22
-	google.golang.org/grpc v1.70.0
+	github.com/pion/webrtc/v4 v4.0.13
+	github.com/spf13/cobra v1.9.1
+	github.com/spf13/viper v1.19.0
+	github.com/zzzhr1990/go-common-entity v0.0.0-20250202070650-1a200048f0d3
+	google.golang.org/grpc v1.71.0
 )
 
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/crackcomm/go-gitignore v0.0.0-20241020182519-7843d2ba8fdf // indirect
-	github.com/dgraph-io/ristretto v1.0.0 // indirect
-	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/gammazero/deque v1.0.0 // indirect
+	github.com/dgraph-io/ristretto/v2 v2.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.4 // indirect
-	github.com/google/flatbuffers v24.3.25+incompatible // indirect
-	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/google/flatbuffers v25.2.10+incompatible // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/ipfs/bbloom v0.0.4 // indirect
-	github.com/ipfs/go-datastore v0.6.0 // indirect
-	github.com/ipfs/go-ipfs-util v0.0.3 // indirect
-	github.com/ipfs/go-ipld-format v0.6.0 // indirect
-	github.com/ipfs/go-ipld-legacy v0.2.1 // indirect
-	github.com/ipfs/go-log/v2 v2.5.1 // indirect
-	github.com/ipfs/go-metrics-interface v0.0.1 // indirect
-	github.com/ipld/go-codec-dagpb v1.6.0 // indirect
-	github.com/ipld/go-ipld-prime v0.21.0 // indirect
-	github.com/jbenet/goprocess v0.1.4 // indirect
-	github.com/klauspost/compress v1.17.11 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
-	github.com/libp2p/go-buffer-pool v0.1.0 // indirect
-	github.com/magiconair/properties v1.8.7 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
+	github.com/magiconair/properties v1.8.9 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/minio/sha256-simd v1.0.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
@@ -62,50 +43,48 @@ require (
 	github.com/multiformats/go-multibase v0.2.0 // indirect
 	github.com/multiformats/go-multihash v0.2.3 // indirect
 	github.com/multiformats/go-varint v0.0.7 // indirect
-	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/pion/datachannel v1.5.10 // indirect
-	github.com/pion/dtls/v3 v3.0.3 // indirect
-	github.com/pion/ice/v4 v4.0.2 // indirect
+	github.com/pion/dtls/v3 v3.0.5 // indirect
+	github.com/pion/ice/v4 v4.0.7 // indirect
 	github.com/pion/interceptor v0.1.37 // indirect
-	github.com/pion/logging v0.2.2 // indirect
+	github.com/pion/logging v0.2.3 // indirect
 	github.com/pion/mdns/v2 v2.0.7 // indirect
 	github.com/pion/randutil v0.1.0 // indirect
 	github.com/pion/rtcp v1.2.15 // indirect
-	github.com/pion/rtp v1.8.10 // indirect
-	github.com/pion/sctp v1.8.35 // indirect
-	github.com/pion/sdp/v3 v3.0.9 // indirect
+	github.com/pion/rtp v1.8.13 // indirect
+	github.com/pion/sctp v1.8.37 // indirect
+	github.com/pion/sdp/v3 v3.0.11 // indirect
 	github.com/pion/srtp/v3 v3.0.4 // indirect
 	github.com/pion/stun/v3 v3.0.0 // indirect
 	github.com/pion/transport/v3 v3.0.7 // indirect
 	github.com/pion/turn/v4 v4.0.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/polydawn/refmt v0.89.0 // indirect
-	github.com/sagikazarmark/locafero v0.3.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sagikazarmark/locafero v0.7.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
-	github.com/spf13/afero v1.10.0 // indirect
-	github.com/spf13/cast v1.5.1 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/afero v1.14.0 // indirect
+	github.com/spf13/cast v1.7.1 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
-	github.com/whyrusleeping/chunker v0.0.0-20181014151217-fe64bd25879f // indirect
 	github.com/wlynxg/anet v0.0.5 // indirect
-	go.opencensus.io v0.24.0 // indirect
-	go.opentelemetry.io/otel v1.32.0 // indirect
-	go.opentelemetry.io/otel/metric v1.32.0 // indirect
-	go.opentelemetry.io/otel/trace v1.32.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel v1.34.0 // indirect
+	go.opentelemetry.io/otel/metric v1.34.0 // indirect
+	go.opentelemetry.io/otel/trace v1.34.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.27.0 // indirect
-	golang.org/x/crypto v0.33.0 // indirect
-	golang.org/x/exp v0.0.0-20241217172543-b2144cdd0a67 // indirect
-	golang.org/x/net v0.35.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/term v0.29.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20241202173237-19429a94021a // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/term v0.30.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250313205543-e70fdf4c4cb4 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	lukechampine.com/blake3 v1.3.0 // indirect
+	lukechampine.com/blake3 v1.4.0 // indirect
 	rsc.io/qr v0.2.0 // indirect
 )
